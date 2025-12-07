2258ed5e5a3698a09119dffe80460cf3a07d6369
Vansh Phalswal <vanshchwdhary@Vanshs-MacBook-Air-2.local>
2025-12-07 14:47:41 +0530

    Deploy admin dashboard

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  define: {
-    "import.meta.env.VITE_API_BASE_URL": JSON.stringify("https://contact-backend-v7b0.onrender.com")
-  },
-  base: "./",
-});
